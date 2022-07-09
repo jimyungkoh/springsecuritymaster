f21159e279f47e28cc97c935b90f0fff302bf4d3
Jimyung Koh <kojimyung1206@naver.com>
2022-07-09 22:20:59 +0900

    [update] fields, constructor
    updated fields and constructor for handling more information

--- src/main/java/com/jimyungkoh/springsecuritymaster/entity/Customer.java
+++ src/main/java/com/jimyungkoh/springsecuritymaster/entity/Customer.java
@@ -1,14 +1,13 @@
 package com.jimyungkoh.springsecuritymaster.entity;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 @RequiredArgsConstructor
 public class Customer {
@@ -16,9 +15,27 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
     private final String email;
 
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonIgnore
     private final String pwd;
 
     private final String role;
+
+    @Column(name = "create_dt")
+    private String createDt;
+
+    public Customer(String name, String email, String mobileNumber, String pwd, String role, String createDt) {
+        this.name = name;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+        this.pwd = pwd;
+        this.role = role;
+        this.createDt = createDt;
+    }
 }
