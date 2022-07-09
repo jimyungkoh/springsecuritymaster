26c10a239ca07c84bfcaaa7a145e61b2a0825da3
Jimyung Koh <kojimyung1206@naver.com>
2022-07-09 22:21:30 +0900

    [add]
    added entity for creating repository

---
+++ src/main/java/com/jimyungkoh/springsecuritymaster/entity/AccountTransactions.java
@@ -0,0 +1,43 @@
+package com.jimyungkoh.springsecuritymaster.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "account_transactions")
+@Getter
+@Setter
+public class AccountTransactions {
+    @Id
+    @Column(name = "transaction_id")
+    private String transactionId;
+
+    @ManyToOne
+    @Column(name="account_number")
+    private Accounts accounts;
+
+    @ManyToOne
+    @Column(name = "customer_id")
+    private Customer customer;
+
+    @Column(name="transaction_dt")
+    private Date transactionDt;
+
+    @Column(name = "transaction_summary")
+    private String transactionSummary;
+
+    @Column(name="transaction_type")
+    private String transactionType;
+
+    @Column(name = "transaction_amt")
+    private int transactionAmt;
+
+    @Column(name = "closing_balance")
+    private int closingBalance;
+
+    @Column(name = "create_dt")
+    private String createDt;
+}
