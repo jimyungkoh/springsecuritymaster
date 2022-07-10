c7bd5377da609d774235b62c9b57810e801b8693
Jimyung Koh <kojimyung1206@naver.com>
2022-07-10 16:29:16 +0900

    [update] change CrudRepo -> JpaRepo
    JpaRepository extends  PagingAndSortingRepository, QueryByExampleExecutor.
    Because  PagingAndSortingRepository extends CrudRepository interface, JpaRepository is more useful

--- src/main/java/com/jimyungkoh/springsecuritymaster/repository/AccountTransactionsRepository.java
+++ src/main/java/com/jimyungkoh/springsecuritymaster/repository/AccountTransactionsRepository.java
@@ -1,13 +1,13 @@
 package com.jimyungkoh.springsecuritymaster.repository;
 
 import com.jimyungkoh.springsecuritymaster.entity.AccountTransactions;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AccountTransactionsRepository extends CrudRepository<AccountTransactions, String> {
+public interface AccountTransactionsRepository extends JpaRepository<AccountTransactions, String> {
 	
 	List<AccountTransactions> findByCustomerIdOrderByTransactionDtDesc(Long customerId);
 
