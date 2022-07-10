c1398692b4fbf4e0e45208dab4f4dfb526da02f5
Jimyung Koh <kojimyung1206@naver.com>
2022-07-10 16:46:09 +0900

    [update] @RequestMapping
    added @RequestMapping on the class level because AccountController handle requests based-on '/myAccount'

--- src/main/java/com/jimyungkoh/springsecuritymaster/web/AccountController.java
+++ src/main/java/com/jimyungkoh/springsecuritymaster/web/AccountController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/myAccount")
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountsRepository accountsRepo;
