579a7f01c51872af6ccc9ed375de41a98a2e5bea
Jimyung Koh <kojimyung1206@naver.com>
2022-07-12 16:24:06 +0900

    [update] sassionManagement(), filters
    set sassionManagement as 'STATELESS' and apply 'JWTTokenValidatorFilter & JWTTokenGeneratorFilter'

--- src/main/java/com/jimyungkoh/springsecuritymaster/security/ProjectSecurityConfig.java
+++ src/main/java/com/jimyungkoh/springsecuritymaster/security/ProjectSecurityConfig.java
@@ -1,22 +1,21 @@
 package com.jimyungkoh.springsecuritymaster.security;
 
-import com.jimyungkoh.springsecuritymaster.filter.AuthoritiesLoggingAfterFilter;
-import com.jimyungkoh.springsecuritymaster.filter.AuthoritiesLoggingAtFilter;
-import com.jimyungkoh.springsecuritymaster.filter.RequestValidationBeforeFilter;
+import com.jimyungkoh.springsecuritymaster.filter.*;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -30,28 +29,17 @@ public class ProjectSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    /*
-     * /myAccount - Secured
-     * /myBalance - Secured
-     * /myLoan - Secured
-     * /myCard - Secured
-     * /notice - Not Secured
-     * /contact - Not Secured
-     * */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        setSessionManagement(http);
         setCors(http);
-
         setCsrf(http);
-
         setHttpFilters(http);
 
-//        String[] authMatchers = new String[]{"/myAccount", "/myBalance", "/myLoans", "/myCards"};
         String[] noAuthMatchers = new String[]{"/notices", "/contact"};
 
         http.authorizeRequests(auth -> auth
-//                        .antMatchers(authMatchers).authenticated()
                         .antMatchers("/myAccount").hasRole("USER")
                         .antMatchers("/myBalance").hasAnyRole("ADMIN", "USER")
                         .antMatchers("/myLoans").hasRole("ROOT")
@@ -65,21 +53,30 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+    private void setSessionManagement(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
     private void setCors(HttpSecurity http) throws Exception {
         http.cors().configurationSource(request -> getCorsConfiguration());
     }
 
     private void setCsrf(HttpSecurity http) throws Exception {
-        http.csrf()
-                .ignoringAntMatchers("/contact")
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.csrf().disable();
+                /*.ignoringAntMatchers("/contact")
+                .csrfTokenRepository(
+                        CookieCsrfTokenRepository.withHttpOnlyFalse());*/
     }
 
     private void setHttpFilters(HttpSecurity http) {
-
         http.addFilterBefore(new RequestValidationBeforeFilter(),
                         BasicAuthenticationFilter.class)
                 .addFilterAfter(new AuthoritiesLoggingAfterFilter(),
+                        BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(),
+                        BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(),
                         BasicAuthenticationFilter.class)
                 .addFilterAt(new AuthoritiesLoggingAtFilter(),
                         BasicAuthenticationFilter.class);
@@ -92,6 +89,7 @@ public class ProjectSecurityConfig {
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setExposedHeaders(List.of("Authorization"));
         config.setMaxAge(3600L);
         return config;
     }
