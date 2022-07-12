649cd1abed6cfd0ba53b413a8e4a1ad8b1ad3fcb
Jimyung Koh <kojimyung1206@naver.com>
2022-07-12 14:43:08 +0900

    [update] set security debeg mode as true
    set security debeg mode as true for seeing how security filters work on

--- src/main/java/com/jimyungkoh/springsecuritymaster/application/SpringSecurityMasterApplication.java
+++ src/main/java/com/jimyungkoh/springsecuritymaster/application/SpringSecurityMasterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @ComponentScans({
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         @ComponentScan("com.jimyungkoh.springsecuritymaster.web")})
 @EntityScan({"com.jimyungkoh.springsecuritymaster.entity"})
 @EnableJpaRepositories({"com.jimyungkoh.springsecuritymaster.repository"})
+@EnableWebSecurity(debug = true)
 public class SpringSecurityMasterApplication {
 
     public static void main(String[] args) {
